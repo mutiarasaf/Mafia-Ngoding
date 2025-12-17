6646e7933cc4c7327398cdf1836416c0ad168bfb
naila <naila.nasywa09@gmail.com>
2025-12-17 13:26:26 +0700

    Menambahkan file app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("📊 Analisis Indikator Pembangunan 34 Provinsi (2014–2024)")
 # ================================
 # 1. LOAD DATA
 # ================================
-df = pd.read_csv("C:/Mafia-Ngoding/data/dataindikatorpembangunan.csv")
+df = pd.read_csv("C:/Mafia-Ngoding/data/data-indikator-pembangunan.csv")
 
 st.subheader("📄 Data Mentah (CSV)")
 st.dataframe(df, use_container_width=True)
